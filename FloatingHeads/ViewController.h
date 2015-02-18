0f278e5f5ffb070231c88f030c81bbc95b45a713
4johnny <4johnny@users.noreply.github.com>
2015-02-18 14:44:38 -0800

    Initial Commit

---
+++ FloatingHeads/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FloatingHeads
+//
+//  Created by Johnny on 2015-02-18.
+//  Copyright (c) 2015 Empath Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
